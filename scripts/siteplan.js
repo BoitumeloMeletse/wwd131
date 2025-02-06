60559f9549789116934f01da186b19616d1b0be3
BoitumeloMeletse <2memusicbookings@gmail.com>
2025-02-06 03:04:45 +0200

    wwe

---
+++ scripts/siteplan.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const themeToggle = document.getElementById("themeToggle");
+    
+    themeToggle.addEventListener("click", function() {
+        document.body.classList.toggle("dark-mode");
+    });
+});
